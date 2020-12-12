bcd2bc3d0b4957732f01540ab2028aa9f6d4b8fe
Emiliano <emitsu.studios@gmail.com>
2020-12-11 23:47:01 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
+// Esta funcion lo que hace es que el codigo espere a que el html termine de cargar antes de ejecutarse
+
 (function(){
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
     
+  /*
+    const logo = document.getElementById('logo');
+
+    const navegacion = document.getElementById('navegacion');
     
-      
-      
+    navegacion.style.display ='none';
+  */
+
+  const blogArt = document.getElementsByClassName('articulo-blog');
     
+  console.log(blogArt);
+  console.log(blogArt[2]);
+
+  const contenido = document.getElementsByClassName('contenido-blog');
+  console.log(contenido);
     
   });
   
